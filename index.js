7574da5fc23f1f6885220657588571078af57857
UnnatiPulla <upulla@purdue.edu>
2018-05-21 17:10:02 -0400

    Added edit button to enable user to edit name of flick

--- index.js
+++ index.js
@@ -17,13 +17,16 @@ class App {
         const item = this.template.cloneNode(true);
         item.classList.remove("template");
         item.dataset.id = flick.id;
-        item
-            .querySelector(".flickName")
-            .textContent = flick.name;
 
+        const nameSpan = item.querySelector(".flickName");
+        nameSpan.textContent = flick.name;
+        nameSpan.addEventListener("keypress", this.saveOnEnter.bind(this, item, flick));
+        
         item.querySelector(".delete.button").addEventListener("click", this.deleteFlick.bind(this, item,flick));
 
         item.querySelector(".fav.button").addEventListener("click", this.favFlick.bind(this, item, flick));
+
+        item.querySelector(".edit.button").addEventListener("click", this.toggleEditable.bind(this, item, flick));
 
         return item;
     }
@@ -43,6 +46,37 @@ class App {
         flick.fav = item.classList.toggle('fav'); //toggle returns true/ false. when on, it returns true, otherwise it returns false. hence we change the prop of object using this!
     }
 
+    toggleEditable(item, flick, ev){
+        const nameField = item.querySelector(".flickName");
+        const btn = item.querySelector(".edit.button");
+        if(nameField.isContentEditable){
+            //make it no longer editable
+            nameField.contentEditable = false;
+            btn.textContent = "edit";
+            btn.classList.remove("success");
+
+            //save changes
+            flick.name = nameField.textContent;
+            //cannot use nameField.value
+
+
+        }
+        else{
+            nameField.contentEditable = true;
+            nameField.focus();
+            btn.textContent = "save";
+            btn.classList.add("success");
+            //make it editable
+        }
+
+    }
+
+    saveOnEnter(item, flick, ev){
+        if(ev.key === "Enter"){
+            this.toggleEditable(item, flick )
+        }
+    }
+
     handleSubmit(ev){
         ev.preventDefault();
         const f = ev.target; //entire form not just button
@@ -51,7 +85,6 @@ class App {
             name: f.flickName.value,
             clickTimes: 0,
             fav: false,
-
         }
         this.flicks.unshift(flick)
         //for the array part
